d5ce8b43c587a75dd1a4abaa6beec419fd06cbcc
chuan2984 <chuanscareer@gmail.com>
2026-03-29 22:01:36 -0700

    changing the setup name to be the name of the fork

--- plugin/init.lua
+++ plugin/init.lua
@@ -8,21 +8,21 @@ local is_windows = wezterm.target_triple == "x86_64-pc-windows-msvc"
 local separator = is_windows and "\\" or "/"
 
 local function init()
-	-- enable_sub_modules()
-	local opts = {
-		auto = true,
-		keywords = { "github", "MLFlexer", "resurrect", "wezterm" },
-	}
-	local plugin_path = dev.setup(opts)
+  -- enable_sub_modules()
+  local opts = {
+    auto = true,
+    keywords = { "github", "chuan2984", "resurrect", "wezterm" },
+  }
+  local plugin_path = dev.setup(opts)
 
-	require("resurrect.state_manager").change_state_save_dir(plugin_path .. separator .. "state" .. separator)
+  require("resurrect.state_manager").change_state_save_dir(plugin_path .. separator .. "state" .. separator)
 
-	-- Export submodules
-	pub.workspace_state = require("resurrect.workspace_state")
-	pub.window_state = require("resurrect.window_state")
-	pub.tab_state = require("resurrect.tab_state")
-	pub.fuzzy_loader = require("resurrect.fuzzy_loader")
-	pub.state_manager = require("resurrect.state_manager")
+  -- Export submodules
+  pub.workspace_state = require("resurrect.workspace_state")
+  pub.window_state = require("resurrect.window_state")
+  pub.tab_state = require("resurrect.tab_state")
+  pub.fuzzy_loader = require("resurrect.fuzzy_loader")
+  pub.state_manager = require("resurrect.state_manager")
 end
 
 init()
